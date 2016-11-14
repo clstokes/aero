7166e9ff98157ac67ecd213ad8e0cb72092acf67
Cameron Stokes <cameron@cameronstokes.com>
2016-11-13 21:30:21 -0800

    Update Google tests and fmt.

--- providers/google.go
+++ providers/google.go
@@ -1,110 +1,110 @@
 package providers
 
 import (
-  "fmt"
-  "github.com/clstokes/aero/structs"
-  "strings"
+	"fmt"
+	"github.com/clstokes/aero/structs"
+	"strings"
 )
 
 type Google struct {
-  Mapping structs.ProviderMapping
+	Mapping structs.ProviderMapping
 }
 
 func (g Google) Name() string {
-  return structs.NAME_GOOGLE
+	return structs.NAME_GOOGLE
 }
 
 func (g Google) IsCurrentProvider() bool {
-  _, err := g.Read(structs.KEY_ADDRESS_PRIVATE)
-  if err != nil {
-    return false
-  }
-  return true
+	_, err := g.Read(structs.KEY_ADDRESS_PRIVATE)
+	if err != nil {
+		return false
+	}
+	return true
 }
 
 func (g Google) Read(s string) (string, error) {
-  metadataItem, exists := g.Mapping.MetadataItems[s]
-  if !exists {
-    return "", fmt.Errorf("No lookup support for key [%v]", s)
-  }
+	metadataItem, exists := g.Mapping.MetadataItems[s]
+	if !exists {
+		return "", fmt.Errorf("No lookup support for key [%v]", s)
+	}
 
-  url := g.Mapping.MetadataAddress + metadataItem.Url
-  value, err := GetMetadata(url, map[string]string{"Metadata-Flavor": "Google"})
+	url := g.Mapping.MetadataAddress + metadataItem.Url
+	value, err := GetMetadata(url, map[string]string{"Metadata-Flavor": "Google"})
 
-  if err != nil {
-    return "", err
-  }
+	if err != nil {
+		return "", err
+	}
 
-  if metadataItem.ParseFunc != nil {
-    parsedValue, err := metadataItem.ParseFunc(value)
-    return parsedValue, err
-  }
+	if metadataItem.ParseFunc != nil {
+		parsedValue, err := metadataItem.ParseFunc(value)
+		return parsedValue, err
+	}
 
-  return value, nil
+	return value, nil
 }
 
 // https://cloud.google.com/compute/docs/metadata
 // Metadata-Flavor: Google
 func InitGoogle(defaults structs.ProviderMapping) structs.Provider {
-  mapping := structs.ProviderMapping{
-    MetadataAddress: "http://169.254.169.254",
-    MetadataItems: map[string]structs.MetadataItem{
-      structs.KEY_ADDRESS_PRIVATE: structs.MetadataItem{
-        Url: "/computeMetadata/v1/instance/network-interfaces/0/ip",
-      },
-      structs.KEY_ADDRESS_PUBLIC: structs.MetadataItem{
-        Url: "/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/external-ip",
-      },
-      structs.KEY_INSTANCE_NAME: structs.MetadataItem{
-        Url: "/computeMetadata/v1/instance/hostname",
-        ParseFunc: func(v interface{}) (string, error) {
-          // value is in form "<name>.c.<project-name>.internal"
-          value := v.(string)
-          valueSplit := strings.Split(value, ".")
+	mapping := structs.ProviderMapping{
+		MetadataAddress: "http://169.254.169.254",
+		MetadataItems: map[string]structs.MetadataItem{
+			structs.KEY_ADDRESS_PRIVATE: structs.MetadataItem{
+				Url: "/computeMetadata/v1/instance/network-interfaces/0/ip",
+			},
+			structs.KEY_ADDRESS_PUBLIC: structs.MetadataItem{
+				Url: "/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/external-ip",
+			},
+			structs.KEY_INSTANCE_NAME: structs.MetadataItem{
+				Url: "/computeMetadata/v1/instance/hostname",
+				ParseFunc: func(v interface{}) (string, error) {
+					// value is in form "<name>.c.<project-name>.internal"
+					value := v.(string)
+					valueSplit := strings.Split(value, ".")
 
-          if len(valueSplit) != 4 {
-            return "", fmt.Errorf("Unparseable value [%s]", value)
-          }
+					if len(valueSplit) != 4 {
+						return "", fmt.Errorf("Unparseable value [%s]", value)
+					}
 
-          return valueSplit[0], nil
-        },
-      },
-      structs.KEY_PROVIDER: structs.MetadataItem{
-        // kind of a hack to not introduce another special field/function
-        Url: "/computeMetadata/v1/instance/hostname",
-        ParseFunc: func(v interface{}) (string, error) {
-          return structs.NAME_GOOGLE, nil
-        },
-      },
-      structs.KEY_ZONE: structs.MetadataItem{
-        Url: "/computeMetadata/v1/instance/zone",
-        ParseFunc: func(v interface{}) (string, error) {
-          // value is in form "projects/<project-id>/zones/<zone>"
-          value := v.(string)
-          valueSplit := strings.Split(value, "/")
+					return valueSplit[0], nil
+				},
+			},
+			structs.KEY_PROVIDER: structs.MetadataItem{
+				// kind of a hack to not introduce another special field/function
+				Url: "/computeMetadata/v1/instance/hostname",
+				ParseFunc: func(v interface{}) (string, error) {
+					return structs.NAME_GOOGLE, nil
+				},
+			},
+			structs.KEY_ZONE: structs.MetadataItem{
+				Url: "/computeMetadata/v1/instance/zone",
+				ParseFunc: func(v interface{}) (string, error) {
+					// value is in form "projects/<project-id>/zones/<zone>"
+					value := v.(string)
+					valueSplit := strings.Split(value, "/")
 
-          if len(valueSplit) != 4 {
-            return "", fmt.Errorf("Unparseable value [%s]", value)
-          }
+					if len(valueSplit) != 4 {
+						return "", fmt.Errorf("Unparseable value [%s]", value)
+					}
 
-          return valueSplit[3], nil
-        },
-      },
-      structs.KEY_REGION: structs.MetadataItem{
-        Url: "/computeMetadata/v1/instance/zone",
-        ParseFunc: func(v interface{}) (string, error) {
-          // value is in form "projects/<project-id>/zones/<zone>"
-          value := v.(string)
-          valueSplit := strings.Split(value, "/")
+					return valueSplit[3], nil
+				},
+			},
+			structs.KEY_REGION: structs.MetadataItem{
+				Url: "/computeMetadata/v1/instance/zone",
+				ParseFunc: func(v interface{}) (string, error) {
+					// value is in form "projects/<project-id>/zones/<zone>"
+					value := v.(string)
+					valueSplit := strings.Split(value, "/")
 
-          // value is in form "<region>-<location><number>-<zone-letter>"
-          zoneValue := valueSplit[3]
-          return fmt.Sprintf("%s", zoneValue[0:len(zoneValue)-2]), nil
-        },
-      },
-    },
-  }
-  ChangeProviderMappingDefaults(&mapping, &defaults)
-  google := Google{Mapping: mapping}
-  return google
+					// value is in form "<region>-<location><number>-<zone-letter>"
+					zoneValue := valueSplit[3]
+					return fmt.Sprintf("%s", zoneValue[0:len(zoneValue)-2]), nil
+				},
+			},
+		},
+	}
+	ChangeProviderMappingDefaults(&mapping, &defaults)
+	google := Google{Mapping: mapping}
+	return google
 }
--- providers/google_test.go
+++ providers/google_test.go
@@ -1,66 +1,66 @@
 package providers
 
 import (
-  "github.com/clstokes/aero/structs"
-  "net/http"
-  "net/http/httptest"
-  "testing"
+	"github.com/clstokes/aero/structs"
+	"net/http"
+	"net/http/httptest"
+	"testing"
 )
 
 const (
-  GOOGLE_DIR_FIXTURES = "test-fixtures/google/"
+	GOOGLE_DIR_FIXTURES = "test-fixtures/google/"
 )
 
 var GOOGLE_TEST_DATA = map[string]string{
-  structs.KEY_ADDRESS_PRIVATE: "10.240.0.2",
-  structs.KEY_ADDRESS_PUBLIC:  "52.207.213.249",
-  structs.KEY_INSTANCE_NAME:   "i-0849c4a43b144116d",
-  structs.KEY_PROVIDER:        "amazon",
-  structs.KEY_REGION:          "us-east-1",
-  structs.KEY_ZONE:            "us-east-1d",
+	structs.KEY_ADDRESS_PRIVATE: "10.240.0.2",
+	structs.KEY_ADDRESS_PUBLIC:  "104.155.153.127",
+	structs.KEY_INSTANCE_NAME:   "main",
+	structs.KEY_PROVIDER:        "google",
+	structs.KEY_REGION:          "us-central1",
+	structs.KEY_ZONE:            "us-central1-a",
 }
 
 func TestGoogleName(t *testing.T) {
-  server, provider := getGoogleServerAndProvider()
-  defer server.Close()
+	server, provider := getGoogleServerAndProvider()
+	defer server.Close()
 
-  value := provider.Name()
-  testValue := structs.NAME_GOOGLE
-  if value != testValue {
-    t.Fatalf("Name from provider [%s] did not match constant [%s].", value, testValue)
-  }
+	value := provider.Name()
+	testValue := structs.NAME_GOOGLE
+	if value != testValue {
+		t.Fatalf("Name from provider [%s] did not match constant [%s].", value, testValue)
+	}
 }
 
 func TestGoogleIsCurrentProvider(t *testing.T) {
-  server, provider := getGoogleServerAndProvider()
-  defer server.Close()
+	server, provider := getGoogleServerAndProvider()
+	defer server.Close()
 
-  if !provider.IsCurrentProvider() {
-    t.Fatalf("Not set as current provider.")
-  }
+	if !provider.IsCurrentProvider() {
+		t.Fatalf("Not set as current provider.")
+	}
 }
 
-func TestGoogleRead(t *testing.T) {
-  server, provider := getGoogleServerAndProvider()
-  defer server.Close()
+func TestGoogleReadAll(t *testing.T) {
+	server, provider := getGoogleServerAndProvider()
+	defer server.Close()
 
-  for key, testValue := range GOOGLE_TEST_DATA {
-    value, err := provider.Read(key)
-    if err != nil {
-      t.Fatalf("err: %s", err)
-    }
+	for key, testValue := range GOOGLE_TEST_DATA {
+		value, err := provider.Read(key)
+		if err != nil {
+			t.Fatalf("err: %s", err)
+		}
 
-    if value != string(testValue) {
-      t.Fatalf("Value from provider [%s] did not match test data [%s].", value, testValue)
-    }
-  }
+		if value != string(testValue) {
+			t.Fatalf("Value from provider [%s] did not match test data [%s].", value, testValue)
+		}
+	}
 
 }
 
 func getGoogleServerAndProvider() (*httptest.Server, structs.Provider) {
-  server := httptest.NewServer(http.FileServer(http.Dir(GOOGLE_DIR_FIXTURES)))
-  mapping := structs.ProviderMapping{
-    MetadataAddress: server.URL,
-  }
-  return server, InitGoogle(mapping)
+	server := httptest.NewServer(http.FileServer(http.Dir(GOOGLE_DIR_FIXTURES)))
+	mapping := structs.ProviderMapping{
+		MetadataAddress: server.URL,
+	}
+	return server, InitGoogle(mapping)
 }
